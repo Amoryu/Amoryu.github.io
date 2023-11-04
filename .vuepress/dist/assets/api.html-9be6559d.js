fc3f20f9935a845f1c048134e788329da2519d99
Amor yu <2014377205@qq.com>
2023-11-04 19:54:31 +0800

    deploy

---
+++ .vuepress/dist/assets/api.html-9be6559d.js
@@ -0,0 +1,1 @@
+const e=JSON.parse('{"key":"v-acdd9360","path":"/docs/theme-reco/api.html","title":"api","lang":"en-US","frontmatter":{"title":"api","date":"2020/05/29"},"headers":[],"git":{"createdTime":1698776902000,"updatedTime":1698776902000,"contributors":[{"name":"Amor yu","email":"2014377205@qq.com","commits":1}]},"filePathRelative":"docs/theme-reco/api.md"}');export{e as data};
--- deploy.sh
+++ deploy.sh
@@ -8,7 +8,7 @@
   cd .vuepress/dist
 
 
-  git init
+  # git init
   git add -A
   git commit -m 'deploy'
 
@@ -18,6 +18,6 @@
   # 如果发布到 https://<USERNAME>.github.io/<REPO>
   # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master:gh-pages
   # 我的配置如下
-  git push -f git@github.com:Amoryu/Amoryu.github.io.git main:gh-pages
+  git push -f https://github.com/Amoryu/amoryu.github.io main:gh-pages
 
   cd -
